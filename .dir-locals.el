aebb855ecc570984bb74fdb7d5044da2c7d37c5d
FiVo <finn.volkel@gmail.com>
2021-08-29 22:49:11 +0200

    working shadow-cljs with clojure-cli version

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((nil . ((cider-clojure-cli-global-options . "-A:dev")
+         (cider-default-cljs-repl . custom)
+         (cider-custom-cljs-repl-init-form . "(do (user/cljs-repl))")
+         (eval . (progn
+                   (make-variable-buffer-local 'cider-jack-in-nrepl-middlewares)
+                   (add-to-list 'cider-jack-in-nrepl-middlewares "shadow.cljs.devtools.server.nrepl/middleware"))))))
